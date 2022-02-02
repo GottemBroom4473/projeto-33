035d0add79d22d6bf6d89cf56adf1e6657783bbd
GottemBroom4473 <94195497+GottemBroom4473@users.noreply.github.com>
2022-02-02 14:51:45 -0300

    Add files via upload

--- balao.js
+++ balao.js
@@ -6,8 +6,14 @@ class balao{
         }
         this.body.x = this.body.x;
         this.body.y = this.body.y;
+
+        this.imageB = loadImage("blue_balloon0.png");
+        this.imageG = loadImage("green_balloon0.png");
+        this.imageP = loadImage("pink_balloon0.png");
+        this.imageR = loadImage("red_balloon0.png");
     }
     display(){
-
+        imageMode(CENTER);
+        
     }
 }
